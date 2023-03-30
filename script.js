5e7a9a8cc21cb7314571b53f3e185cd21648176b
BadHard <BadHard101@yandex.ru>
2023-03-30 07:12:53 +0300

    4 Points added - DONE TODO: 2. Description field onClick 3. Maps of points 4. onClick maps changing logic 5. Design (+) add more points

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 
 let center = [55.883589, 37.621490];
 let park_sveta = [55.887294, 37.595885];
+let ba_park = [55.867521, 37.683005];
+let altufevo_estate = [55.908547, 37.584851];
+let lianozovsky_park = [55.900138, 37.565189];
+
 
 function init() {
     let map = new ymaps.Map('map-test', {
@@ -20,8 +24,19 @@ function init() {
         '</div>'
     );*/
 
+
+    // ПАРК СВЕТА
+    // Создаем метку Парка света
+    // (параметры текста, картинки, их позиций относительно центра)
     let m1_park_sveta = new ymaps.Placemark(park_sveta, {
-        iconContent: `<div class="my-custom-icon__text" style="white-space: nowrap; font-size: 16px; font-family: Arial Black,serif;
+        iconContent: `<div class="my-custom-icon__text" 
+        style="white-space: nowrap; 
+        font-size: 20px; 
+        /*font-family: 'Segoe Print';*/
+        /*font-family: 'Comic Sans MS';*/
+        /*font-family: Bahnschrift;*/
+        /*font-family: Calibri;*/
+        font-family: 'MS Reference Sans Serif', serif;
         text-shadow: 1px 0 2px #ffffff, 
         0 1px 2px #ffffff, 
         -1px 0 2px #ffffff, 
@@ -32,14 +47,121 @@ function init() {
         iconImageHref: 'imgs/park_sveta.png',
         iconImageSize: [100, 100],
         iconImageOffset: [-50, -60],
-        iconContentOffset: [0, 73], // Смещение HTML-элемента относительно центра иконки
+        iconContentOffset: [-5, 73], // Смещение HTML-элемента относительно центра иконки
         //iconContentLayout: MyCustomLayoutClass // Свой макет HTML-элемента
     });
-
-    let box1 = document.querySelector('#box');
+    // Отображаем меню с информацией о точке и меняем карту на более подробную
+    let menu1 = document.querySelector('#box');
     m1_park_sveta.events.add('click', function (e){
-        box1.style.display = "flex";
+        menu1.style.display = "flex";
     })
+
+
+    // БАБУШКИНСКИЙ ПАРК
+    // Создаем метку Бабушкинского парка
+    // (параметры текста, картинки, их позиций относительно центра)
+    let m2_ba_park = new ymaps.Placemark(ba_park, {
+        iconContent: `<div class="my-custom-icon__text" 
+        style="white-space: nowrap; 
+        font-size: 20px; 
+        /*font-family: 'Segoe Print';*/
+        /*font-family: 'Comic Sans MS';*/
+        /*font-family: Bahnschrift;*/
+        /*font-family: Calibri;*/
+        font-family: 'MS Reference Sans Serif', serif;
+        text-shadow: 1px 0 2px #ffffff, 
+        0 1px 2px #ffffff, 
+        -1px 0 2px #ffffff, 
+        0 -1px 2px #ffffff;
+        font-weight: bold; ">Бабушкинский парк</div>`
+    }, {
+        iconLayout: 'default#imageWithContent',
+        iconImageHref: 'imgs/ba_park.png',
+        iconImageSize: [100, 100],
+        iconImageOffset: [-50, -60],
+        iconContentOffset: [-40, 73], // Смещение HTML-элемента относительно центра иконки
+        //iconContentLayout: MyCustomLayoutClass // Свой макет HTML-элемента
+    });
+    // Отображаем меню с информацией о точке и меняем карту на более подробную
+    let menu2 = document.querySelector('#box');
+    m2_ba_park.events.add('click', function (e){
+        menu2.style.display = "flex";
+    })
+
+
+    // УСАДЬБА АЛТУФЬЕВО
+    // Создаем метку Усадьбы Алтуфьево
+    // (параметры текста, картинки, их позиций относительно центра)
+    let m3_Altufyevo_Estate = new ymaps.Placemark(altufevo_estate, {
+        iconContent: `<div class="my-custom-icon__text" 
+        style="white-space: nowrap; 
+        font-size: 20px; 
+        /*font-family: 'Segoe Print';*/
+        /*font-family: 'Comic Sans MS';*/
+        /*font-family: Bahnschrift;*/
+        /*font-family: Calibri;*/
+        font-family: 'MS Reference Sans Serif', serif;
+        text-shadow: 1px 0 2px #ffffff, 
+        0 1px 2px #ffffff, 
+        -1px 0 2px #ffffff, 
+        0 -1px 2px #ffffff;
+        font-weight: bold; ">Усадьба Алтуфьево</div>`
+    }, {
+        iconLayout: 'default#imageWithContent',
+        iconImageHref: 'imgs/altufevo_estate.png',
+        iconImageSize: [100, 100],
+        iconImageOffset: [-50, -60],
+        iconContentOffset: [-40, 73], // Смещение HTML-элемента относительно центра иконки
+        //iconContentLayout: MyCustomLayoutClass // Свой макет HTML-элемента
+    });
+    // Отображаем меню с информацией о точке
+    // и меняем карту на более подробную при нажатии
+    let menu3 = document.querySelector('#box');
+    m3_Altufyevo_Estate.events.add('click', function (e){
+        menu3.style.display = "flex";
+    })
+
+
+    // ЛИАНОЗОВСКИЙ ПАРК
+    // Создаем метку Лианозовского парка
+    // (параметры текста, картинки, их позиций относительно центра)
+    let m4_lianozovsky_park = new ymaps.Placemark(lianozovsky_park, {
+        iconContent: `<div class="my-custom-icon__text" 
+        style="white-space: nowrap; 
+        font-size: 20px; 
+        /*font-family: 'Segoe Print';*/
+        /*font-family: 'Comic Sans MS';*/
+        /*font-family: Bahnschrift;*/
+        /*font-family: Calibri;*/
+        font-family: 'MS Reference Sans Serif', serif;
+        text-shadow: 1px 0 2px #ffffff, 
+        0 1px 2px #ffffff, 
+        -1px 0 2px #ffffff, 
+        0 -1px 2px #ffffff;
+        font-weight: bold; ">Лианозовский парк</div>`
+    }, {
+        iconLayout: 'default#imageWithContent',
+        iconImageHref: 'imgs/lianozovsky_park.png',
+        iconImageSize: [100, 100],
+        iconImageOffset: [-50, -60],
+        iconContentOffset: [-40, 73], // Смещение HTML-элемента относительно центра иконки
+        //iconContentLayout: MyCustomLayoutClass // Свой макет HTML-элемента
+    });
+    // Отображаем меню с информацией о точке
+    // и меняем карту на более подробную при нажатии
+    let menu4 = document.querySelector('#box');
+    m4_lianozovsky_park.events.add('click', function (e){
+        menu4.style.display = "flex";
+    })
+
+
+
+    // Добавляем все созданные метки на карту
+    map.geoObjects.add(m1_park_sveta);
+    map.geoObjects.add(m2_ba_park);
+    map.geoObjects.add(m3_Altufyevo_Estate);
+    map.geoObjects.add(m4_lianozovsky_park);
+
 
 
     map.controls.remove('geolocationControl'); // удаляем геолокацию
@@ -50,8 +172,6 @@ function init() {
     map.controls.remove('zoomControl'); // удаляем контрол зуммирования
     map.controls.remove('rulerControl'); // удаляем контрол правил
     // map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
-
-    map.geoObjects.add(m1_park_sveta);
 }
 
 ymaps.ready(init);
